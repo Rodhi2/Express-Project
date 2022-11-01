fba3486bdc2c557148694c33d380823b2bec8fd3
Rodhi Faisal Mufid <111524042+Rodhi2@users.noreply.github.com>
2022-11-01 22:11:56 +0700

    Delete Express Project directory

--- Express Project/src/helpers/util.mjs
+++
@@ -1,8 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-const importJson = async (path) => {
-  const jsonFile = await readFile(new URL(path, import.meta.url));
-  return JSON.parse(jsonFile);
-};
-
-export { importJson };
